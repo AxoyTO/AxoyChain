cf29dac372bbfd92c1e7528a3e853965a0d92644
Tevfik Oguzhan Aksoy <toaxoy@protonmail.com>
2022-01-29 23:18:31 +0300

    (+) Proof of Work prototype

--- block.js
+++ block.js
@@ -1,13 +1,16 @@
-const { GENESIS_DATA } = require('./config');
+const { GENESIS_DATA, MINE_RATE } = require('./config');
 const crypto = require('crypto');
 const cryptoHash = require('./crypto-hash');
+const { time } = require('console');
 
 class Block {
-    constructor({ timestamp, lastHash, hash, data }) {
+    constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
+        this.nonce = nonce;
+        this.difficulty = difficulty;
     }
 
     static genesis() {
@@ -15,15 +18,28 @@ class Block {
     }
 
     static mineBlock({ lastBlock, data }) {
-        const timestamp = Date.now();
-        const lastHash = lastBlock.hash;
+        let hash, timestamp;
 
-        return new this({
-            timestamp,
-            lastHash,
-            data,
-            hash: cryptoHash(timestamp, lastHash, data)
-        });
+        // const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        let nonce = 0;
+        const { difficulty } = lastBlock;
+
+        do {
+            nonce++;
+            timestamp = Date.now();
+            hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+        } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+
+        return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
+    }
+
+    static adjustDifficulty({ originalBlock, timestamp }) {
+        const { difficulty } = originalBlock;
+
+        if ((timestamp - originalBlock.timestamp) > MINE_RATE) return difficulty - 1;
+
+        return difficulty + 1;
     }
 }
 
