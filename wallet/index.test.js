00758286203f0dc5954ef07079f52aa32c83223b
Tevfik Oguzhan Aksoy <toaxoy@protonmail.com>
2022-02-02 21:23:56 +0300

    Tx update and Wallet createTransaction

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 const Wallet = require('./index')
-const { verifySignature } = require('../util')
+const { verifySignature } = require('../util');
+const Transaction = require('./transaction');
 
 describe('Wallet', () => {
     let wallet;
@@ -39,6 +40,37 @@ describe('Wallet', () => {
                 data,
                 signature: new Wallet().sign(data)
             })).toBe(false)
+        })
+    })
+
+    describe('createTransaction()', () => {
+        describe('and the amount exceeds the balance', () => {
+            it('throws the error', () => {
+                expect(() => wallet.createTransaction({ amount: 999999, recipient: "foo-recipient" }))
+                    .toThrow('Amount exceeds balance')
+            })
+        })
+
+        describe('and the amount is valid', () => {
+            let transaction, amount, recipient
+
+            beforeEach(() => {
+                amount = 50
+                recipient = "foo-recipient"
+                transaction = wallet.createTransaction({ amount, recipient })
+            })
+
+            it('creates an instance of `Transaction`', () => {
+                expect(transaction instanceof Transaction).toBe(true)
+            })
+
+            it('matches the transaction input with the wallet', () => {
+                expect(transaction.input.address).toEqual(wallet.publicKey)
+            })
+
+            it('outputs the amount to the recipient', () => {
+                expect(transaction.outputMap[recipient]).toEqual(amount)
+            })
         })
     })
 })
