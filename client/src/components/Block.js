6b8d815a5ab4373528d55d684344847362df129a
Tevfik Oguzhan Aksoy <toaxoy@protonmail.com>
2022-03-19 20:56:32 +0300

    Redacted Block

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react'
-import { Button } from 'react-bootstrap'
-import Transaction from './Transaction'
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import Transaction from './Transaction';
 
 class Block extends Component {
     state = { displayTransaction: false };
+
     toggleTransaction = () => {
         this.setState({ displayTransaction: !this.state.displayTransaction });
     }
 
     get displayTransaction() {
         const { data } = this.props.block;
+
         const stringifiedData = JSON.stringify(data);
 
         const dataDisplay = stringifiedData.length > 35 ?
@@ -39,33 +41,33 @@ class Block extends Component {
             )
         }
 
-        return <div>
-            <div>Data: {dataDisplay}</div>
-            <Button
-                variant="danger"
-                size="sm"
-                onClick={this.toggleTransaction}
-            >
-                Show More
-            </Button>
-        </div>
+        return (
+            <div>
+                <div>Data: {dataDisplay}</div>
+                <Button
+                    variant="danger"
+                    size="sm"
+                    onClick={this.toggleTransaction}
+                >
+                    Show More
+                </Button>
+            </div>
+        );
     }
 
     render() {
-
         const { timestamp, hash } = this.props.block;
-        const hashDisplay = `${hash.substring(0, 15)}...`;
 
+        const hashDisplay = `${hash.substring(0, 15)}...`;
 
         return (
             <div className='Block'>
                 <div>Hash: {hashDisplay}</div>
-                <div>Timestamp: {new Date(timestamp).toLocaleString('en-GB')}</div>
+                <div>Timestamp: {new Date(timestamp).toLocaleString()}</div>
                 {this.displayTransaction}
             </div>
-        )
+        );
     }
 };
-
 
 export default Block;
