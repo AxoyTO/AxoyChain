1ee3a208e1ae300db4d33f8279a35679db27155d
Tevfik Oguzhan Aksoy <toaxoy@protonmail.com>
2022-03-19 16:13:43 +0300

    Added convenient tx, tx pool & mining functionality

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Blocks from './Blocks'
-import logo from '../assets/logo.png'
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 class App extends Component {
     state = { walletInfo: {} }
 
     componentDidMount() {
-        fetch('http://localhost:3000/api/wallet-info')
+        fetch(`${document.location.origin}/api/wallet-info`)
             .then(response => response.json())
             .then(json => this.setState({ walletInfo: json }));
     }
@@ -17,17 +17,19 @@ class App extends Component {
         return (
             <div className='App'>
                 <img className='logo' src={logo}></img>
-                <br /><br />
                 <div>
-                    Welcome to the Blockchain...
+                    <br />
+                    Welcome to the Axoy Chain...
                 </div>
                 <br />
+                <div><Link to='/blocks'>Blocks</Link></div>
+                <div><Link to='/conduct-transaction'>Conduct a transaction</Link></div>
+                <div><Link to='/transaction-pool'>Transaction Pool</Link></div>
                 <div className='WalletInfo'>
+                    <br />
                     <div>Address: {address}</div>
                     <div>Balance: {balance}</div>
                 </div>
-                <Blocks />
-                <br />
             </div>
         );
     }
