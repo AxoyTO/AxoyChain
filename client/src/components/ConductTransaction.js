27ece8fcd6b9e76df9d4337c56d7f4ee9c460fdd
Tevfik Oguzhan Aksoy <toaxoy@protonmail.com>
2022-03-20 16:23:09 +0300

    Added tooltip for known addresses

--- client/src/components/ConductTransaction.js
+++ client/src/components/ConductTransaction.js
@@ -43,7 +43,9 @@ class ConductTransaction extends Component {
                 </div>
                 <h3>Conduct a Transaction</h3>
                 <br />
-                <h4>Known Addresses</h4>
+                <div class="tooltip fade" data-title="Wallet addresses with conducted transaction histories are known addresses">
+                    <h4>Known Addresses</h4>
+                </div>
                 {
                     this.state.knownAddresses.map(knownAddress => {
                         return (
@@ -83,6 +85,7 @@ class ConductTransaction extends Component {
                     </Form>
                 </div>
                 <div>
+                    <br />
                     <Button
                         variant="danger"
                         onClick={this.conductTransaction}
